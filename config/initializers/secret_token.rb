4fbc642f177b94d38ba5479ae79bbe4d4250c4a1
Jarod Adair <adairjk@gmail.com>
2014-08-31 11:54:05 -0500

    installing rails 3 framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonkeyBarrel::Application.config.secret_token = '4b6e7e4fe6068a9f3aa70965808109117e9c41928d420a50190f85c91cfc3db15672e2d8e1d79373e6cde575854c35f8f465129ac560c37a7015c9b0d6754503'
