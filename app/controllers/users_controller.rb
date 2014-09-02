b07ac1f5eb63e698272b036295e45e992d5047ba
Jarod Adair <adairjk@gmail.com>
2014-09-01 21:01:40 -0500

    add delete button to user list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @user = User.find(params[:id])
     if User.destroy(params[:id])
       redirect_to users_path, :notice => 'User has been deleted'
